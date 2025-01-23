9b14b90b346dad5067377d063ab86aa8430c4f1f
Jay <jayd1903@gmail.com>
2025-01-23 12:07:25 -0800

    draw and drag cells

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+let isMouseDown = false;
 
-cellRows = 18;
-cellCols = 24;
+canvas.addEventListener('mousedown', function(e) {
+    isMouseDown = true;
+    handleMouseDown(canvas, e);
+});
+
+canvas.addEventListener('mouseup', function(e) {
+    isMouseDown = false;
+});
+
+canvas.addEventListener('mousemove', function(evt) {
+    if (isMouseDown) {
+        handleMouseDown(canvas, evt);
+    }
+});
+
+const cellRows = 18;
+const cellCols = 24;
 let cellWidth = canvas.width / cellCols;
 let cellHeight = canvas.height / cellRows;
-cells = new Array(cellRows);
+let cells = new Array(cellRows);
 for (let i = 0; i < cellRows; i++) {
     cells[i] = new Array(cellCols);
     for (let j = 0; j < cellCols; j++) {
-        cells[i][j] = Math.floor(Math.random() * 2);
+        cells[i][j] = 0;
     }
 }
 
-function anim () {
-    ctx.fillStyle = "white";
+anim();
+function anim() {
+    ctx.fillStyle = "blue";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < cellRows; i++) {
         for (let j = 0; j < cellCols; j++) {
             ctx.fillStyle = blackOrWhiteColorString(cells[i][j]);
-            ctx.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+            ctx.fillRect(j * cellWidth, i * cellHeight, cellWidth - 1, cellHeight - 1);
         }
     }
-    //sleep for 1000 ms
-    setTimeout(anim, 400);
-}
-
-function grabRandomColorString() {
-    randomRed = Math.floor(Math.random() * 255);
-    randomGreen = Math.floor(Math.random() * 255);
-    randomBlue = Math.floor(Math.random() * 255);
-    return "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+    requestAnimationFrame(anim);
 }
 
 function blackOrWhiteColorString(x) {
@@ -39,4 +48,22 @@ function blackOrWhiteColorString(x) {
     } else {
         return "black";
     }
+}
+
+function handleMouseDown(canvas, event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const row = Math.floor(y / cellHeight);
+    const col = Math.floor(x / cellWidth);
+    cells[row][col] = 1;
+}
+
+function startAnimation() {
+    for (let i = 0; i < cellRows; i++) {
+        for (let j = 0; j < cellCols; j++) {
+            cells[i][j] = 1 - cells[i][j];
+        }
+    }
+    setTimeout(startAnimation, 700);
 }
