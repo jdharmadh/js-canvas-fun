c5a89547bfacc38ad35a1c6db894c8a9c58ae537
Jay <jayd1903@gmail.com>
2025-01-25 20:39:36 -0800

    drawing pieces

--- tetris/script.js
+++ tetris/script.js
@@ -1,11 +1,9 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-boardRows = 20;
-boardCols = 10;
+const boardRows = 20;
+const boardCols = 10;
+const squareSize = canvas.width / boardCols
 
 board = new Array(boardRows);
 for (let i = 0; i < boardRows; i++) {
@@ -15,6 +13,33 @@ for (let i = 0; i < boardRows; i++) {
   }
 }
 
-function gameLoop() {}
+function setPiece(piece, row, col) {
+  console.log(piece)
+  gottenPiece = getPiece(piece);
+  console.log(gottenPiece)
+  for (let i = 0; i < gottenPiece.length; i++) {
+    for (let j = 0; j < gottenPiece[0].length; j++) {
+      board[row + i][col + j] += gottenPiece[i][j];
+    }
+  }
+}
 
+function drawBoard() {
+  for (let i = 0; i < boardRows; i++) {
+    for (let j = 0; j < boardCols; j++) {
+      if (board[i][j] == 1){
+        ctx.fillStyle = "orange";
+        ctx.fillRect(i * squareSize, j * squareSize, squareSize, squareSize)
+      }
+    }
+  }
+}
+
+function gameLoop() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  drawBoard();
+}
+
+setPiece(randomPiece(), 0, 0);
 setInterval(gameLoop, 10);
