44c5a1e1d2fad98add3863516fd51d19b93818df
Jay <jayd1903@gmail.com>
2025-01-31 15:48:07 -0800

    jumping over obstacle

--- dino/script.js
+++ dino/script.js
@@ -1,41 +1,83 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-const dinoGravity = -10;
+const dinoGravity = -18;
 const tickSpeed = 20;
 
 const dinoWidth = 20;
 const dinoHeight = 80;
-const dinoX = 20;
+const dinoX = 60;
 dinoY = canvas.height - dinoHeight;
 dinoYVelocity = 0;
 
+hasLost = false;
 
+obstacles = [];
+
+function getRandomObstacle() {
+  return {
+    x: canvas.width,
+    y: canvas.height - 40,
+    width: 40,
+    height: 40,
+    speed: 300,
+  };
+}
 
 function draw() {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    ctx.fillStyle = 'red';
-    ctx.fillRect(dinoX, dinoY, dinoWidth, dinoHeight);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(dinoX, dinoY, dinoWidth, dinoHeight);
+
+  ctx.fillStyle = "orange";
+  for (let i = 0; i < obstacles.length; i++) {
+    ctx.fillRect(
+      obstacles[i].x,
+      obstacles[i].y,
+      obstacles[i].width,
+      obstacles[i].height
+    );
+  }
 }
 
 function update(ms) {
-    dinoY -= dinoYVelocity * ms/1000;
-    dinoYVelocity += dinoGravity;
-    if (dinoY > canvas.height - dinoHeight) {
-        dinoY = canvas.height - dinoHeight;
-        dinoYVelocity = 0;
+  dinoY -= (dinoYVelocity * ms) / 1000;
+  dinoYVelocity += dinoGravity;
+  if (dinoY > canvas.height - dinoHeight) {
+    dinoY = canvas.height - dinoHeight;
+    dinoYVelocity = 0;
+  }
+  if (obstacles.length === 0) {
+    obstacles.push(getRandomObstacle());
+  }
+  for (let i = 0; i < obstacles.length; i++) {
+    obstacles[i].x -= obstacles[i].speed * (ms / 1000);
+    if (obstacles[i].x + obstacles[i].width < 0) {
+      obstacles.splice(i, 1);
     }
+
+    if (
+      dinoX < obstacles[i].x + obstacles[i].width &&
+      dinoX + dinoWidth > obstacles[i].x &&
+      dinoY < obstacles[i].y + obstacles[i].height &&
+      dinoY + dinoHeight > obstacles[i].y
+    ) {
+      obstacles = [];
+      dinoY = canvas.height - dinoHeight;
+      dinoYVelocity = 0;
+    }
+  }
 }
 
-document.addEventListener('keydown', function(event) {
-    if (event.code === 'ArrowUp') {
-        dinoYVelocity = 256;
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.code === "ArrowUp") {
+    if (dinoY === canvas.height - dinoHeight) dinoYVelocity = 400;
+  }
 });
 
-setInterval(function() {
-    update(tickSpeed);
-    draw();
+setInterval(function () {
+  update(tickSpeed);
+  draw();
 }, tickSpeed);
