48f2dd7213744e5d5ca20414da4aa9da43058d5f
Jay <jayd1903@gmail.com>
2025-01-31 16:09:01 -0800

    game

--- dino/script.js
+++ dino/script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 const dinoGravity = -18;
 const tickSpeed = 20;
 
-const dinoWidth = 20;
+const dinoWidth = 80;
 const dinoHeight = 80;
 const dinoX = 60;
 dinoY = canvas.height - dinoHeight;
@@ -14,47 +14,59 @@ hasLost = false;
 
 obstacles = [];
 
+score = 0;
+
+const dinosaurImage = new Image();
+dinosaurImage.src = "./dino.webp";
+
+const cactusImage = new Image();
+cactusImage.src = "./cactus3.webp";
+
 function getRandomObstacle() {
-  return {
-    x: canvas.width,
-    y: canvas.height - 40,
-    width: 40,
-    height: 40,
-    speed: 300,
-  };
+    return {
+        x: canvas.width,
+        y: canvas.height - 40,
+        width: 40,
+        height: 40,
+        speed: 300 + score * 50,
+    };
 }
 
 function draw() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "red";
-  ctx.fillRect(dinoX, dinoY, dinoWidth, dinoHeight);
+    ctx.drawImage(dinosaurImage, dinoX, dinoY, dinoWidth, dinoHeight);
 
-  ctx.fillStyle = "orange";
-  for (let i = 0; i < obstacles.length; i++) {
-    ctx.fillRect(
-      obstacles[i].x,
-      obstacles[i].y,
-      obstacles[i].width,
-      obstacles[i].height
-    );
-  }
+    ctx.fillStyle = "orange";
+    for (let i = 0; i < obstacles.length; i++) {
+        ctx.drawImage(
+            cactusImage,
+            obstacles[i].x,
+            obstacles[i].y,
+            obstacles[i].width,
+            obstacles[i].height
+        );
+    }
 
-  if (hasLost) {
-    console.log("You lost!");
-    const despairImage = new Image();
-    despairImage.src = "./despair.png";
-    despairImage.onload = function () {
-      ctx.drawImage(
-        despairImage,
-        canvas.width / 2 - 50,
-        canvas.height / 2 - 50,
-        100,
-        100
-      );
-    };
-  }
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("Score: " + score, 10, 30);
+
+    if (hasLost) {
+        console.log("You lost!");
+        const despairImage = new Image();
+        despairImage.src = "./despair.png";
+        despairImage.onload = function () {
+            ctx.drawImage(
+                despairImage,
+                canvas.width / 2 - 50,
+                canvas.height / 2 - 50,
+                100,
+                100
+            );
+        };
+    }
 }
 
 function update(ms) {
@@ -71,6 +83,7 @@ function update(ms) {
     obstacles[i].x -= obstacles[i].speed * (ms / 1000);
     if (obstacles[i].x + obstacles[i].width < 0) {
       obstacles.splice(i, 1);
+        score++;
     }
 
     if (
@@ -92,6 +105,7 @@ document.addEventListener("keydown", function (event) {
     if (hasLost) {
       dinoY = canvas.height - dinoHeight;
       dinoYVelocity = 0;
+      score = 0;
       hasLost = false;
       obstacles = [];
     }
